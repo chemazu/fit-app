0da4ee1d0ad73c6c6dc08cec3e09436418a3b054
chemazu <chukwuemekachemazu@gmail.com>
2022-11-06 11:27:41 +0100

    first home page view created

--- src/pages/Home/sub-pages/components/ForYouCard.tsx
+++ src/pages/Home/sub-pages/components/ForYouCard.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './ForYouCard.scss'
+import squat from "../../../../assets/img/bench.svg"
 
  
 
 export default function ForYouCard() {
   return (
     <div className="for-you-card">
-        <img src="" alt="squat" />
+        <img src={squat} alt="squat" />
         <div className="content">
             <p>Try a guided run</p>
             <h3>First Fartlek Run</h3>
--- src/pages/Home/sub-pages/mini-home.tsx
+++ src/pages/Home/sub-pages/mini-home.tsx
@@ -15,16 +15,15 @@ export default function HomePage() {
       <p className="heading-item">Weekly Routine</p>
       <WeeklyRoutine />
 
-      {/* <p className="heading-item">For you</p>
+      <div className="start-container">
+        <h2>START</h2>
+      </div>
+      <p className="heading-item">For you</p>
+
       <div className="for-you-card-wrapper">
         <ForYouCard />
         <ForYouCard />
         <ForYouCard />
-        
-      </div> */}
-
-      <div className="start-container">
-        <h2>START</h2>
       </div>
     </div>
   );
